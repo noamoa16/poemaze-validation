f1b6b138362c71737a77ade37ac5bfedd753ea4c
noamoa16 <122087446+noamoa16@users.noreply.github.com>
2023-01-12 20:06:49 +0900

    Add files via upload

---
+++ split.h
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<string> split(string str, const char c) {
+	replace(str.begin(), str.end(), c, ' ');
+	istringstream iss(str);
+
+	vector<string> ret;
+	while (true) {
+		string buf;
+		iss >> buf;
+		if (buf == "") break;
+		ret.push_back(buf);
+	}
+	return ret;
+}
